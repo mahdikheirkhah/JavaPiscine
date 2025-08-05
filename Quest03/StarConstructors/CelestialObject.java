9e464ce2e02f5a2b9c61e31087414eb7024dc239
mahdikheirkhah <mohammad.kheirkhah@gritlab.ax>
2025-08-05 20:57:30 +0300

    Star constructors

---
+++ Quest03/StarConstructors/CelestialObject.java
@@ -0,0 +1,18 @@
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+    public CelestialObject(){
+        x = 0.0;
+        y = 0.0;
+        z = 0.0;
+        name = "Soleil";
+    }
+    public CelestialObject(String name,double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+}
