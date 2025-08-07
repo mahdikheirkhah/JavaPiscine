6e5aa7d3f316c8e472a849a799eda6fed1493244
mahdikheirkhah <mohammad.kheirkhah@gritlab.ax>
2025-08-07 06:47:20 +0300

    stream collect

---
+++ Quest05/StreamCollect/StreamCollect.java
@@ -0,0 +1,17 @@
+import java.util.stream.Stream;
+import java.util.Map;
+import java.util.List;
+import java.util.Optional;
+public class StreamCollect {
+    public static Map<Character, List<String>> mapByFirstLetter(Stream<String> s) {
+        // your code here
+    }
+
+    public static Map<Integer, Optional<Integer>> getMaxByModulo4(Stream<Integer> s) {
+        // your code here
+    }
+
+    public static String orderAndConcatWithSharp(Stream<String> s) {
+        // your code here
+    }
+}
